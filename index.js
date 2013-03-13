017706d0dfa65aa3714f2cf9aa5e0246b2fae304
Youssef Garas <ygaras@irisoftonline.com>
2013-03-13 17:03:01 -0400

    Move auto connect to CM module

--- index.js
+++ index.js
@@ -4,15 +4,3 @@ module.exports = {
     AutoModels : require('./lib/auto_models')
 };
 
-
-// Establish connections to defined databases
-if (typeof 'Basbosa' !== 'undefined' && Basbosa('Config').get('db')) {
-  if (Array.isArray(Basbosa('Config').get('db'))) {
-    Basbosa('Config').get('db').forEach(function(connection) {
-      B('Logger').debug(connection);
-      Basbosa('Cm').add(connection.name, connection);
-    });
-  } else if (typeof Basbosa('Config').get('db') === 'object') {
-    Basbosa('Cm').add('default', Basbosa('Config').get('db'));
-  }
-}
