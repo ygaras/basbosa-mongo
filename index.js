b8dcf56c1204917a0483d3b1949b0f49d3308465
Youssef Garas <ygaras@irisoftonline.com>
2013-03-16 11:44:03 -0400

    Move loading connections to index

--- index.js
+++ index.js
@@ -3,15 +3,3 @@ module.exports = {
     BasbosaModel : require('./lib/model'),
     AutoModels : require('./lib/auto_models')
 };
-
-
-// Establish connections to defined databases
-if (typeof 'Basbosa' !== 'undefined' && Basbosa('Config').get('db')) {
-  if (Array.isArray(Basbosa('Config').get('db'))) {
-    Basbosa('Config').get('db').forEach(function(connection) {
-      Basbosa('Cm').add(connection.name, connection);
-    });
-  } else if (typeof Basbosa('Config').get('db') === 'object') {
-    Basbosa('Cm').add('default', Basbosa('Config').get('db'));
-  }
-}
